ef2357dbf863914302ec887b567845c92b3f91ff
Donald Mennerich <don.mennerich@nyu.edu>
2018-05-29 14:16:42 -0400

    adding average support for time of day

--- app/helpers/Stats.scala
+++ app/helpers/Stats.scala
@@ -5,21 +5,53 @@ import scala.math.BigDecimal
 import models.Entry
 
 case class Averages(total: Double, morning: Double, noon: Double, evening: Double)
+case class Counts(morning: Int, noon: Int, evening: Int)
 
 trait StatsSupport {
   def getAverages(xs: List[Entry]): Averages = {
     val size = xs.size.toDouble
+    
     @tailrec
-    def inner(xs: List[Entry], accum: Int): Averages = {
-      xs match {
-        case x :: tail => {
-          inner(tail, accum + x.reading)
+    def inner(entries: List[Entry], averages: Averages, counts: Counts): Averages = {
+      entries match {
+        case entry :: tail => { 
+          entry.readingTime match {
+            case 1 => {
+              inner(
+                tail, 
+                new Averages(averages.total + entry.reading, averages.morning + entry.reading, averages.noon, averages.evening),
+                new Counts(counts.morning + 1, counts.noon, counts.evening))
+            }
+            
+            case 2 => {
+              inner(
+                tail, 
+                new Averages(averages.total + entry.reading, averages.morning, averages.noon + entry.reading, averages.evening),
+                new Counts(counts.morning, counts.noon + 1, counts.evening))
+            }
+
+            case 3 => {
+              inner(
+                tail, 
+                new Averages(averages.total + entry.reading, averages.morning, averages.noon, averages.evening + entry.reading),
+                new Counts(counts.morning, counts.noon, counts.evening + 1))    
+            }
+
+            case _ => throw new Exception
+          }
         }
-        case Nil => new Averages((round(accum.toDouble / size)), 0.0, 0.0, 0.0)
+        case Nil => new Averages(
+          round(averages.total / size), 
+          round(averages.morning / counts.morning), 
+          round(averages.noon / counts.noon), 
+          round(averages.evening / counts.evening))
       }
     }
-    inner(xs, 0)
+    
+    inner(xs, Averages(0.0, 0.0, 0.0, 0.0), Counts(0,0,0))
   }
 
   private def round(double: Double): Double = { BigDecimal(double).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble }
+
 }
+
